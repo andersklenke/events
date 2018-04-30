64a4d3d96605ff3870ded8b596ec7d78ba47486d
Anders Klenke <anders_klenke@icloud.com>
2018-04-30 21:04:03 +0200

    Add Event type

---
+++ app/graphql/types/event.rb
@@ -0,0 +1,9 @@
+class Types::Event < Types::BaseObject
+  description <<~DESC
+    The event type contains different fields with information about a
+    specific event such as name and participants.
+  DESC
+
+  field :id, ID, 'Unique identifier of the event', null: false
+  field :name, String, 'Name of the event', null: false
+end
--- app/graphql/types/query.rb
+++ app/graphql/types/query.rb
@@ -1,2 +1,8 @@
 class Types::Query < Types::BaseObject
+  field :events, [Types::Event], 'Get all events', null: true
+  field :users, [Types::User], null: true
+
+  def events
+    ::Event.all
+  end
 end
