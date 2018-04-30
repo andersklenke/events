77d49cf79bd454b4284bfc0ec567db9bf61b7ea0
Anders Klenke <anders_klenke@icloud.com>
2018-04-30 21:28:04 +0200

    Add User type

--- app/graphql/types/query.rb
+++ app/graphql/types/query.rb
@@ -1,8 +1,12 @@
 class Types::Query < Types::BaseObject
   field :events, [Types::Event], 'Get all events', null: true
-  field :users, [Types::User], null: true
+  field :users, [Types::User], 'Get all users', null: true
 
   def events
     ::Event.all
+  end
+
+  def users
+    ::User.all
   end
 end
---
+++ app/graphql/types/user.rb
@@ -0,0 +1,9 @@
+class Types::User < Types::BaseObject
+  description <<~DESC
+    The user type contains different fields with information about a
+    specific user such as name and events the user signed up for.
+  DESC
+
+  field :id, ID, 'Unique identifier of the user', null: false
+  field :name, String, 'Name of the user', null: false
+end
