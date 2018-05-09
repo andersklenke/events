bdd4a9b8af3b629f654e6315a22c7e5280dbabb7
Anders Klenke <anders_klenke@icloud.com>
2018-05-09 09:02:48 +0200

    Add `findEvent` field to the query type

--- app/graphql/types/query.rb
+++ app/graphql/types/query.rb
@@ -1,6 +1,9 @@
 class Types::Query < Types::BaseObject
   field :events, [Types::Event], 'Get all events', null: true
   field :users, [Types::User], 'Get all users', null: true
+  field :find_event, Types::Event, "Find an event from it's ID", null: true do
+    argument :id, ID, required: true
+  end
 
   def events
     ::Event.all
@@ -8,5 +11,9 @@ class Types::Query < Types::BaseObject
 
   def users
     ::User.all
+  end
+
+  def find_event(id:)
+    ::Event.find_by(id: id)
   end
 end
